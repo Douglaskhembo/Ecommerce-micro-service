08eef92dc2eb412289c61ee6dbb3069a736e686f
Douglas <khembo.douglas.juma@gmail.com>
2025-04-03 22:02:08 +0300

    notification impl

---
+++ services/payment/src/main/java/com/ecommerce/record/PaymentNotificationRequest.java
@@ -0,0 +1,15 @@
+package com.ecommerce.record;
+
+import com.ecommerce.enumeration.PaymentMethod;
+
+import java.math.BigDecimal;
+
+public record PaymentNotificationRequest(
+        String orderReference,
+        BigDecimal amount,
+        PaymentMethod paymentMethod,
+        String customerFirstName,
+        String customerLastName,
+        String customerEmail
+) {
+}
