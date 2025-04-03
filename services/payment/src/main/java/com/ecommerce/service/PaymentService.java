5aedea906ad9985c31986677cfcd38408c2fa287
Douglas <khembo.douglas.juma@gmail.com>
2025-04-03 21:44:00 +0300

    payment service

--- services/payment/src/main/java/com/ecommerce/service/PaymentService.java
+++ services/payment/src/main/java/com/ecommerce/service/PaymentService.java
@@ -1,6 +1,8 @@
 package com.ecommerce.service;
 
+import com.ecommerce.mapper.PaymentMapper;
 import com.ecommerce.record.PaymentRequest;
+import com.ecommerce.repository.PaymentRepository;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,7 +10,11 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class PaymentService {
+
+    private final PaymentRepository repository;
+    private final PaymentMapper mapper;
     public Integer createPayment(@Valid PaymentRequest paymentRequest) {
+        var payment = repository.save(mapper.toPayment(paymentRequest));
         return null;
     }
 }
